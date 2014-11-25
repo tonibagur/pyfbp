38c4a024043bc72a0f03156a8f41d95b0def8ea3
Toni Bagur <toni.bagur@gmail.com>
2014-11-25 10:32:46 +0100

    first version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pyfbp',
+      version='0.1',
+      description='Flow Based Programming Paradigm in Python',
+      author='Toni Bagur',
+      author_email='toni.bagur@coneptum.com',
+      url='https://github.com/tonibagur/pyfbp',
+      packages=['pyfbp'],
+     )
