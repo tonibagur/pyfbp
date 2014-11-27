aa365c0735f550f4dce064588c295ef2208cf5a0
Toni Bagur <toni.bagur@gmail.com>
2014-11-27 15:30:23 +0100

    first version with better inout support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyfbp',
-      version='0.1.1',
+      version='0.1.2',
       description='Flow Based Programming Paradigm in Python',
       author='Toni Bagur',
       author_email='toni.bagur@coneptum.com',
